1f10b8e301f2f865b3a500dc9196087c2eac1003
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-29 20:46:53 +0200

    Update script.js
    getting inputs and passing a control

--- DOM-manipulation/script.js
+++ DOM-manipulation/script.js
@@ -6,5 +6,11 @@ function handleLoad() {
 }
 
 function handleClick() {
-  
+    let inputs = document.getElementsByTagName('input');
+
+    if (inputs[0].value !== "" && inputs[1].value !== "") {
+        let value1 = parseInt(inputs.namedItem("value1").value);
+        let value2 = parseInt(inputs.namedItem("value2").value);
+        let output = document.querySelector("output");
+    }
 }
