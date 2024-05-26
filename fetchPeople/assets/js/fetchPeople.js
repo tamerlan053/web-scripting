ef4e29076560b8fd653caa3faa156e7f27279fed
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-05-26 13:24:27 +0200

    Update fetchPeople.js
    event listeners added

--- fetchPeople/assets/js/fetchPeople.js
+++ fetchPeople/assets/js/fetchPeople.js
@@ -3,5 +3,14 @@
 window.addEventListener('load', loaded);
 
 function loaded() {
-  
+    let buttonGetAllPeople = document.getElementById('button_get_all_people');
+    buttonGetAllPeople.addEventListener("click", handleGetAllPeople);
+    let buttonGetPerson = document.getElementById('button_get_person');
+    buttonGetPerson.addEventListener("click", handleGetPerson);
+    let buttonPost = document.getElementById('button_post_person');
+    buttonPost.addEventListener("click", handlePostPerson);
+    let buttonGetPersonByName = document.getElementById('button_get_person_by_name');
+    buttonGetPersons=ByName.addEventListener("click", handleGetPersonByName);
+    let buttonPutPerson = document.getElementById('button_put_person');
+    buttonPutPerson.addEventListener("click", handlePutPerson);
 }
