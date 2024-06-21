32f3bbfcb4e2142ec41069885ab81f61c6fde746
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-06-21 23:27:09 +0200

    Update script.js
    init eventhandler added

--- guess-game/js/script.js
+++ guess-game/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function init() {
+    let button = document.getElementById('rollBtn');
+    button.addEventListener('click', rollDice);
+}
+
+function rollDice() {
+  
+}
+
+window.addEventListener('load', init);
