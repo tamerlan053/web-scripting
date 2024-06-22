b3de8b12ef8e432f6fe749d8e7b4b02994f7a02b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-06-22 23:29:00 +0200

    Update script.js
    output preparation

--- guess-game/js/script.js
+++ guess-game/js/script.js
@@ -8,6 +8,9 @@ function init() {
 function rollDice() {
     let url = 'http://localhost:3000/letters/';
     let divDice = document.getElementById('dice');
+    let wordInput = document.getElementById('wordInput');
+    makeElementEmpty(divDice);
+    makeElementEmpty(wordInput);
 }
 
 window.addEventListener('load', init);
